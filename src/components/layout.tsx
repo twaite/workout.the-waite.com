7967c21f6d3b48395eed6fe175c1a6c838424ec5
Tim Waite <tawaite@gmail.com>
2026-01-11 17:15:35 -0600

    upgrade: htmx 1.9.10 -> 2.0.8

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children?: any }) {
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Workout App</title>
-        <script src="https://unpkg.com/htmx.org@1.9.10"></script>
+        <script src="https://unpkg.com/htmx.org@2.0.8"></script>
       </head>
       <body>
         <div id="app">{children}</div>
